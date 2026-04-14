17f0e53ca60fcc993ec7c59f0e0f9c472fa71395
DavidB <david.bernard@fms-ea.com>
2026-04-14 15:44:51 +0200

    first commit

---
+++ tests/models/test_course.py
@@ -0,0 +1,25 @@
+import pytest
+
+from models.address import Address
+from models.course import Course
+from models.person import Person
+from models.student import Student
+from models.teacher import Teacher
+from datetime import date
+
+def test_course():
+    course = Course("Français", date(2024, 1, 29), date(2024, 2, 16))
+
+    assert course.name == "Français"
+    assert course.start_date == date(2024, 1, 29)
+    assert course.end_date == date(2024, 2, 16)
+    assert course.teacher is None
+
+
+def test_set_teacher():
+    sut = Course("Français", date(2024, 1, 29), date(2024, 2, 16))
+    teacher = Teacher("Dupont", "Paul", 35, date(2024, 1, 29))
+
+    sut.set_teacher(teacher)
+
+    assert sut.teacher == teacher
